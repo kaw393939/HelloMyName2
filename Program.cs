2b8da56075d0253ed3ef0e8306dc472c311a55c2
kaw393939 <keith@webizly.com>
2019-11-25 15:13:55 -0500

    Feature: adde favorite ice cream

--- Program.cs
+++ Program.cs
@@ -9,7 +9,9 @@ namespace HelloMyName
             Console.WriteLine("Hello World!");
             string fname = "Keith";
             string lname = "Williams";
+            string icecream = "cookie dough";
             Console.WriteLine("Hello my name is {0} {1}", fname, lname);
+            Console.WriteLine("My favorite ice cream is {0}", icecream);
 
         }
     }
